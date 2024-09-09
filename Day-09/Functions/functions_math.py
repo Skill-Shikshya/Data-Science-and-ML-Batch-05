2dceee170797c75295b9a9124aefd87eef7786ea
Shailesh Adhikari <shailesh.adhikari98@gmail.com>
2024-09-09 08:53:39 +0545

    Update Day09 : USer defined functions and documentation

---
+++ Day-09/Functions/functions_math.py
@@ -0,0 +1,14 @@
+def square(n=4):
+    """
+    Find a squre any given number
+
+    Parameters:
+    -----------
+    n : int or float, default = 4
+    User input number for squaring
+
+    Return:
+    -------
+    return square of a number
+    """
+    return n**2
